fb2adb754d4229f47ce2084da9d7ac43274b3a93
aaronmhr <aaronmhr@gmail.com>
2019-10-13 10:40:14 +0200

    Initial Commit

---
+++ N26BC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  N26BC
+//
+//  Created by Aaron Huánuco on 13/10/2019.
+//  Copyright © 2019 Aaron Huánuco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
