e5b76555966afa05d0f0b88f1f3af02900e9b1aa
aaronmhr <aaronmhr@gmail.com>
2019-10-15 01:58:04 +0200

    Test URLSessionClient

--- NetworkingTests/Networking/URLSessioClientTests.swift
+++ NetworkingTests/Networking/URLSessioClientTests.swift
@@ -64,33 +64,88 @@ class URLSessionClientTests: XCTestCase {
             self?.assertFailure(result: result, error: networkingError)
         }
     }
+    
+    func testFetchResources_whenRequestSuccedsButWrongObject_returnsNetworkingErrorDecodingError() {
+        let (sut, session) = makeSUTRegularDecoding()
+        let networkingError = NetworkingError.couldNotBuildURL
+        let (url, httpResponse) = givenDecodingError()
+        session.whenDealingWith(.success(httpResponse))
+        
+        sut.fetchResources(url: url) { [weak self] (result: Result<MockModelResponse, NetworkingError>) in
+            self?.assertFailure(result: result, error: networkingError)
+        }
+    }
+    
+    func testFetchResources_whenThereSuccedes_returnsDecodedObject() {
+        let (sut, session) = makeSUT()
+        let property = "A"
+        let anotherProperty = "B"
+        let (url, httpResponse) = givenSuccessfulRequets(property: property, anotherProperty: anotherProperty)
+        let object = MockModelResponse(property: property, anotherProperty: anotherProperty)
+        session.whenDealingWith(.success(httpResponse))
+
+        sut.fetchResources(url: url) { [weak self] (result: Result<MockModelResponse, NetworkingError>) in
+            self?.assertSuccess(result: result, object: object)
+        }
+    }
+    
+    func testFetchResources_canHandleOrdinaryError() {
+        let url = URL(string: "https://apple.com")!
+        let error = NSError(domain: "Unidentified Error at \(#file)", code: 0, userInfo: nil)
+        let networkingError = NetworkingError.other("\(error.code)")
+        let (sut, session) = makeSUT()
+        session.whenDealingWith(.failure(error))
+        
+        sut.fetchResources(url: url) { [weak self] (result: Result<MockModelResponse, NetworkingError>) in
+            self?.assertFailure(result: result, error: networkingError)
+        }
+    }
+    
     // MARK: - Helpers
     private func makeSUT() -> (URLSessionClient, URLSessionSpy) {
         let session = URLSessionSpy()
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let decoder = getSnakeCaseAdapterJSONDecoder()
         return (URLSessionClient(session: session, decoder: decoder), session)
     }
     
-    private func assertFailure(result: Result<MockModelResponse, NetworkingError>, error: NetworkingError) {
+    private func makeSUTRegularDecoding() -> (URLSessionClient, URLSessionSpy) {
+        let session = URLSessionSpy()
+        let decoder = getRegularJSONDecoder()
+        return (URLSessionClient(session: session, decoder: decoder), session)
+    }
+    
+    private func getRegularJSONDecoder() -> JSONDecoder {
+        return JSONDecoder()
+    }
+    
+    private func getSnakeCaseAdapterJSONDecoder() -> JSONDecoder {
+        let decoder = getRegularJSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
+    
+    private func assertFailure(result: Result<MockModelResponse, NetworkingError>, error: NetworkingError, file: StaticString = #file, line: UInt = #line) {
         switch result {
         case .failure(let error):
             XCTAssertEqual(error, error)
             XCTAssertEqual(error.description, error.description)
         case .success:
-            XCTFail()
+            XCTFail("Unidentified", file: file, line: line)
         }
     }
     
-//    private func assertSuccess(result: Result<MockModelResponse, NetworkingError>, error: NetworkingError) {
-//        switch result {
-//        case .failure(let error):
-//            XCTAssertEqual(error, error)
-//            XCTAssertEqual(error.description, error.description)
-//        case .success:
-//            XCTFail()
-//        }
-//    }
+    private func assertSuccess(result: Result<MockModelResponse, NetworkingError>, object: MockModelResponse, file: StaticString = #file, line: UInt = #line) {
+        switch result {
+        case .failure:
+            XCTFail("Unidentified", file: file, line: line)
+        case .success(let response):
+            XCTAssertEqual(response, object)
+        }
+    }
+    
+    private func makeSimpleJSON(_ dictionary: [String: Any]) -> Data {
+        return try! JSONSerialization.data(withJSONObject: dictionary)
+    }
     
     private func givenValidURLWithNonHTTPResponse() -> (url: URL, response: (URLResponse, Data)) {
         let url = URL(string: "https://apple.com")!
@@ -129,6 +184,28 @@ class URLSessionClientTests: XCTestCase {
         let networkingError = NetworkingError.clientError("\(statusCode)")
         return (url, tuple, networkingError)
     }
+    
+    private func givenDecodingError() -> (url: URL, response: (URLResponse, Data)) {
+        let url = URL(string: "https://apple.com")!
+        let statusCode = 200
+        let urlResponse = HTTPURLResponse(url: url, statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+        let dict: [String: Any] = ["property" : "A", "another_property" : "B"]
+        let data = makeSimpleJSON(dict)
+        let tuple: (URLResponse, Data) = (urlResponse, data)
+        return (url, tuple)
+    }
+    
+    private func givenSuccessfulRequets(property: String, anotherProperty: String) -> (url: URL, response: (URLResponse, Data)) {
+        let url = URL(string: "https://apple.com")!
+        let statusCode = 200
+        let urlResponse = HTTPURLResponse(url: url, statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+        let dict: [String: Any] = ["property" : "\(property)", "another_property" : "\(anotherProperty)" ]
+        let data = makeSimpleJSON(dict)
+        let tuple: (URLResponse, Data) = (urlResponse, data)
+        return (url, tuple)
+    }
+
+    
     // MARK: - Auxiliary Objects
     private class URLSessionSpy: URLSessionProtocol {
         private var result: RemoteResult = .failure(NSError(domain: "", code: 0, userInfo: nil))
@@ -143,7 +220,8 @@ class URLSessionClientTests: XCTestCase {
         }
     }
     
-    struct MockModelResponse: Decodable {
-        
+    struct MockModelResponse: Decodable, Equatable {
+        let property: String
+        let anotherProperty: String
     }
 }
